d686f1865248f9140f7407f639254a861ebe84a1
derrickweru <derrickweru73@gmail.com>
2026-06-13 12:08:02 +0300

    fix: update expense.js

--- expense.js
+++ expense.js
@@ -1,43 +1,42 @@
-// core logic using oop  & function programming
+// Core Logic using OOP & Functional Programming
 export class ExpenseTracker {
     constructor() {
-        // Loading data from local storage or fallback to empty array
+        // Load data from LocalStorage or fallback to empty array
         this.expenses = JSON.parse(localStorage.getItem('expenses')) || [];
     }
-    //Functional programming :keeping  data pure by  spreading the array
+
+    // Functional programming: Keep data pure by spreading the array
     getExpenses() {
         return [...this.expenses];
     }
 
-    addExpenses(description, amount, category) {
+    addExpense(description, amount, category) {
         const newExpense = {
             id: Date.now().toString(),
             description,
             amount: parseFloat(amount),
             category
         };
-
-        // spread operator to add new item without mutating state directly
+        // Spread operator to add new item without mutating state directly
         this.expenses = [...this.expenses, newExpense];
         this.saveToStorage();
         return newExpense;
     }
 
-    // Destructuring in parameter: targetting id to remove it
-    removeExpenses(id) {
-        // Functional programming: filtering out target id cleanly
+    // Destructuring targeting item ID to remove it
+    removeExpense(id) {
         this.expenses = this.expenses.filter(expense => expense.id !== id);
         this.saveToStorage();
     }
 
-    // Functional programming : Reducing array to a single value
+    // Functional programming: Reduce array to single value
     calculateTotal(expensesList = this.expenses) {
         return expensesList.reduce((sum, { amount }) => sum + amount, 0);
     }
 
     filterByCategory(category) {
         if (category === 'All') return this.getExpenses();
-        return this.expenses.filter(expense => expense.category);
+        return this.expenses.filter(expense => expense.category === category);
     }
 
     saveToStorage() {
